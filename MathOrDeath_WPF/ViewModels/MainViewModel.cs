b1cdf37bce3bce3ac223a32469142c3cfa70c1d8
Praktikum <Praktikum@EV-NB-LAP099>
2024-09-23 13:44:32 +0200

    Projektdateien hinzufügen.

---
+++ MathOrDeath_WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOrDeath_WPF.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _input;
+
+        [ObservableProperty]
+        private string _equation;
+
+        [ObservableProperty]
+        private bool _isCorrect;
+
+        [ObservableProperty]
+        private double _CorrectValue;
+    }
+}
